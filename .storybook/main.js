c018270cbfd0228619c2c32964e33db1a97a922f
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2022-11-13 12:34:54 -0500

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,48 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
+// const path = require('path')
+
+// module.exports = {
+//   "stories": [
+//     "../src/**/*.stories.mdx",
+//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/preset-create-react-app"
+//   ],
+//   webpackFinal: async (config) => {
+//     config.module.rules.push({
+//       test: /\,css&/,
+//       use: [
+//         {
+//           loader: 'postcss-loader',
+//           options: {
+//             ident: 'postcss',
+//             plugins: [
+//               require('tailwindcss'),
+//               require('autoprefixer')
+//             ]
+//           }
+//         }
+//       ],
+//       include: path.resolve(__dirname, '../'),
+//     })
+//     return config
+//   }
+// }
